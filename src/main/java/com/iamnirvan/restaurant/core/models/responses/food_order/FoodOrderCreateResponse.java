a40f7c08078165c96587d3c928160e54f84a0100
Shalin <nirvan.work@gmail.com>
2024-10-15 04:56:16 +0530

    fix order issues

--- src/main/java/com/iamnirvan/restaurant/core/models/responses/food_order/FoodOrderCreateResponse.java
+++ src/main/java/com/iamnirvan/restaurant/core/models/responses/food_order/FoodOrderCreateResponse.java
@@ -1,8 +1,6 @@
 package com.iamnirvan.restaurant.core.models.responses.food_order;
 
 import com.iamnirvan.restaurant.core.enums.EStatus;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,11 +9,10 @@ import java.time.OffsetDateTime;
 @Data
 @Builder
 public class FoodOrderCreateResponse {
+    private Long id;
     private String notes;
-    @NotNull(message = "Cart id is required")
-    @Min(value = 1, message = "A valid cart id is required")
     private Long cartId;
-    private OffsetDateTime date;
     private EStatus status;
     private Double total;
+    private OffsetDateTime date;
 }
--- src/main/java/com/iamnirvan/restaurant/core/services/impl/FoodOrderService.java
+++ src/main/java/com/iamnirvan/restaurant/core/services/impl/FoodOrderService.java
@@ -1,8 +1,10 @@
 package com.iamnirvan.restaurant.core.services.impl;
 
+import com.iamnirvan.restaurant.core.enums.EActiveStatus;
 import com.iamnirvan.restaurant.core.enums.EStatus;
 import com.iamnirvan.restaurant.core.exceptions.NotFoundException;
 import com.iamnirvan.restaurant.core.models.entities.Cart;
+import com.iamnirvan.restaurant.core.models.entities.Customer;
 import com.iamnirvan.restaurant.core.models.entities.DishPortionCart;
 import com.iamnirvan.restaurant.core.models.entities.FoodOrder;
 import com.iamnirvan.restaurant.core.models.requests.food_order.FoodOrderCreateRequest;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -26,16 +29,23 @@ public class FoodOrderService implements IFoodOrderService {
     private final CartRepository cartRepository;
     private final FoodOrderRepository foodOrderRepository;
 
+    /**
+     * Places an order based on the provided list of food order requests.
+     *
+     * @param requests a list of FoodOrderCreateRequest objects containing the details of the orders to be placed
+     * @return a list of FoodOrderCreateResponse objects containing the details of the placed orders
+     * @throws NotFoundException if the cart with the specified ID does not exist
+     */
     @Override
     @Transactional
     public List<FoodOrderCreateResponse> placeOrder(List<FoodOrderCreateRequest> requests) {
         final List<FoodOrderCreateResponse> result = new ArrayList<>();
+
         for (FoodOrderCreateRequest request : requests) {
             Cart cart = cartRepository.findById(request.getCartId())
                     .orElseThrow(() -> new NotFoundException(String.format("Cart with id %d does not exist", request.getCartId())));
 
-            // Get the items (dish portion ids) from the cart
-            // Access the dish, its portion and quantity
+            // TODO: get the total from the cart's total value field (needs to be added. represents the cost of all items in the cart + taxes, etc..)
             double total = 0;
             for (DishPortionCart dishPortionCart : cart.getDishPortionCarts()) {
                 total += (dishPortionCart.getDishPortion().getPrice() * dishPortionCart.getQuantity());
@@ -45,7 +55,6 @@ public class FoodOrderService implements IFoodOrderService {
                     .notes(request.getNotes())
                     .status(EStatus.IN_PROGRESS)
                     .total(total)
-//                    .customer(cart.getCustomer())
                     .date(OffsetDateTime.now())
                     .cart(cart)
                     .build();
@@ -55,14 +64,41 @@ public class FoodOrderService implements IFoodOrderService {
             cartRepository.save(cart);
             log.debug(String.format("Created order: %s", foodOrder));
 
-            result.add(FoodOrderCreateResponse.builder()
-                    .notes(request.getNotes())
-                    .cartId(cart.getId())
-                    .date(foodOrder.getDate())
-                    .status(foodOrder.getStatus())
-                    .total(foodOrder.getTotal())
-                    .build());
+            // Deactivate the old cart
+            cart.setStatus(EActiveStatus.INACTIVE);
+            cartRepository.save(cart);
+            log.debug("deactivated the old cart...");
+
+            // Make a new active cart for the customer
+            Customer customer = cart.getCustomer();
+            Set<Cart> customerCarts = customer.getCart();
+
+            Cart newCart = Cart.builder()
+                    .customer(customer)
+                    .status(EActiveStatus.ACTIVE)
+                    .build();
+            cartRepository.save(newCart);
+            log.debug("created new active cart for customer");
+
+            customerCarts.add(newCart);
+            customer.setCart(customerCarts);
+            log.debug("updated customer's carts");
+
+            result.add(Parser.toFoodOrderCreateResponse(foodOrder));
         }
         return result;
+    }
+
+    public static class Parser {
+        public static FoodOrderCreateResponse toFoodOrderCreateResponse(FoodOrder foodOrder) {
+            return FoodOrderCreateResponse.builder()
+                    .id(foodOrder.getId())
+                    .notes(foodOrder.getNotes())
+                    .cartId(foodOrder.getCart().getId())
+                    .status(foodOrder.getStatus())
+                    .total(foodOrder.getTotal())
+                    .date(foodOrder.getDate())
+                    .build();
+        }
     }
 }
