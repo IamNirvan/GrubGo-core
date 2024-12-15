5231e39bf560823f14700d621928b0df27cbc650
Shalin Kulawardane <nirvan.work@gmail.com>
2024-12-16 03:01:17 +0530

    fix cors issue

--- src/main/java/com/iamnirvan/restaurant/core/controllers/LoginController.java
+++ src/main/java/com/iamnirvan/restaurant/core/controllers/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/login")
---
+++ src/main/java/com/iamnirvan/restaurant/core/controllers/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.iamnirvan.restaurant.core.controllers.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:5173"); // Add frontend origin
+        config.addAllowedHeader("*"); // Allow all headers
+        config.addAllowedMethod("*"); // Allow all methods (GET, POST, etc.)
+        config.setAllowCredentials(true); // Allow cookies if needed
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
